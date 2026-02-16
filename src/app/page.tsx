fab5529f6a0f395e329b2e87b8e5c4bef6eb0575
Nkechi Echeta <jecheta@academic.rrc.ca>
2026-02-15 23:39:42 -0600

    Connect Projects section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
 }
